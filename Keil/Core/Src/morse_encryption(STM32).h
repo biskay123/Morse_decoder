8632606e9682a382e26184601e9d8879aaa2f847
NazarZe <zelinskijnazar0@gmail.com>
2024-02-01 10:18:59 +0200

    added .h

--- Keil/Core/Src/morse_encryption(STM32).h
+++ Keil/Core/Src/morse_encryption(STM32).h
@@ -1,1 +1,9 @@
-#pragma once
+// morse.h
+
+#ifndef MORSE_H
+#define MORSE_H
+
+// Функція для конвертації тексту в код Морзе та повернення його у вигляді рядка
+const char* textToMorse(char text[], int size);
+
+#endif // MORSE_H
