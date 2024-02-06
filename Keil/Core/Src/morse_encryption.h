180909b81d717eb1d4bab609273b09844809e392
NazarZe <zelinskijnazar0@gmail.com>
2024-02-06 09:59:29 +0200

    Кодування Морзе перероблене

--- Keil/Core/Src/morse_encryption.h
+++ Keil/Core/Src/morse_encryption.h
@@ -1,9 +1,6 @@
 #ifndef MORSE_H
 #define MORSE_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #define MAX_ENCODED_LENGTH 256
 // Функція для конвертації тексту в код Морзе та повернення його у вигляді масива
